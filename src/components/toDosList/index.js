fe3dbd89bb4a7a57597d0296760ff95e12fbbadc
Jesse Pires <jessepires2010@gmail.com>
2020-06-30 01:34:13 -0300

    Refactoring the reducers

--- src/components/toDosList/index.js
+++ src/components/toDosList/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleTodo } from '../../redux-flow/reducers/todos/actionCreators';
+import * as filterActions from '../../redux-flow/reducers/visibilityFilter/actions';
 
-const ToDosList = ({ todos, handleToggleTodo }) => (
+const ToDosList = ({ todos, activeFilter,  handleToggleTodo }) => (
   <ul>
-    {todos.map((todo) => (
+    {getVisibleTodos(todos, activeFilter).map((todo) => (
         <li 
           key={ todo.id }
           style={{ textDecoration: todo.completed ? 'line-through' :'none' }}
@@ -17,8 +18,19 @@ const ToDosList = ({ todos, handleToggleTodo }) => (
   </ul>
 );
 
+const getVisibleTodos = (todos, activeFilter) => {
+  const filterItems = {
+    [filterActions.SHOW_ALL]: todos,
+    [filterActions.SHOW_COMPLETED]: todos.filter((todo) => todo.completed),
+    [filterActions.SHOW_ACTIVE]: todos.filter((todo) => !todo.completed)
+  };
+
+  return filterItems[activeFilter];
+}
+
 const mapStateToProps = (state) => ({
-  todos: state.todos
+  todos: state.todos,
+  activeFilter: state.visibilityFilter
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/redux-flow/reducers/todos/index.js
+++ src/redux-flow/reducers/todos/index.js
@@ -1,25 +1,26 @@
 import { ADD_TODO, TOGGLE_TODO } from './actions';
+import createReducer from '../createReducer';
 
 export const initialState = [];
 
-const Todos = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO: 
-      return state.concat({
-        id: action.payload.id,
-        text: action.payload.text,
-        completed: false,
-      })
+const Todos = createReducer(initialState, {
+  [ADD_TODO]: (state, action) => (
+    state.concat({
+      id: action.payload,
+      text: action.payload.text,
+      completed: false
+    })
+  ),
 
-    case TOGGLE_TODO:
-      return state.map((todo) => {
-        return todo.id !== action.payload.id 
-          ? todo
-          : { ...todo, completed: !todo.completed } //return todo with completed value changed
-      });
-
-    default: return state
-  }
-};
+  [TOGGLE_TODO]: (state, action) => state.map((todo) => (
+    todo.id !== action.payload.id 
+      ? todo
+      : {
+        ...todo,
+        completed: !todo.completed
+      }
+    )
+  )
+});
 
 export default Todos;
